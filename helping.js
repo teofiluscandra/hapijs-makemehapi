3dcba23001873790763432afd1a91044528ee833
Teofilus Candra <teofiluschandra@gmail.com>
2018-08-25 15:21:56 +0800

    add helper

---
+++ helping.js
@@ -0,0 +1,37 @@
+const Path = require('path')
+const Hapi = require('hapi')
+const Vision = require('vision')
+const Handlebars = require('handlebars')
+
+;(async () => {
+    try {
+        const server = new Hapi.Server({
+            host: 'localhost',
+            port: Number(process.argv[2] || 8080)
+        })
+
+        await server.register(Vision)
+
+        server.views({
+            engines: {
+                html: Handlebars
+            },
+            path: Path.join(__dirname,'templates'),
+            helpersPath: Path.join(__dirname, 'helpers')
+        })
+
+        server.route({
+            path: '/',
+            method: 'GET',
+            handler: {
+                view: 'template.html'
+            }
+        })
+
+        await server.start()
+
+        console.log(`Server running at ${server.info.uri}`)
+    } catch (error) {
+        console.log(error)
+    }
+})();
